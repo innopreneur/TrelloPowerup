8997e873ae6946b334e728d4195b9cd520183c86
Glitch (far-apparel) <none>
2018-03-13 21:10:52 +0000

    🕶🏩 Updated with Glitch

---
+++ public/js/client.js
@@ -0,0 +1,196 @@
+/* global TrelloPowerUp */
+
+// we can access Bluebird Promises as follows
+var Promise = TrelloPowerUp.Promise;
+
+/*
+
+Trello Data Access
+
+The following methods show all allowed fields, you only need to include those you want.
+They all return promises that resolve to an object with the requested fields.
+
+Get information about the current board
+t.board('id', 'name', 'url', 'shortLink', 'members')
+
+Get information about the current list (only available when a specific list is in context)
+So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
+t.list('id', 'name', 'cards')
+
+Get information about all open lists on the current board
+t.lists('id', 'name', 'cards')
+
+Get information about the current card (only available when a specific card is in context)
+So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
+t.card('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
+
+Get information about all open cards on the current board
+t.cards('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
+
+Get information about the current active Trello member
+t.member('id', 'fullName', 'username')
+
+For access to the rest of Trello's data, you'll need to use the RESTful API. This will require you to ask the
+user to authorize your Power-Up to access Trello on their behalf. We've included an example of how to
+do this in the `🔑 Authorization Capabilities 🗝` section at the bottom.
+
+*/
+
+/*
+
+Storing/Retrieving Your Own Data
+
+Your Power-Up is afforded 4096 chars of space per scope/visibility
+The following methods return Promises.
+
+Storing data follows the format: t.set('scope', 'visibility', 'key', 'value')
+With the scopes, you can only store data at the 'card' scope when a card is in scope
+So for example in the context of 'card-badges' or 'attachment-sections', but not 'board-badges' or 'show-settings'
+Also keep in mind storing at the 'organization' scope will only work if the active user is a member of the team
+
+Information that is private to the current user, such as tokens should be stored using 'private' at the 'member' scope
+
+t.set('organization', 'private', 'key', 'value');
+t.set('board', 'private', 'key', 'value');
+t.set('card', 'private', 'key', 'value');
+t.set('member', 'private', 'key', 'value');
+
+Information that should be available to all users of the Power-Up should be stored as 'shared'
+
+t.set('organization', 'shared', 'key', 'value');
+t.set('board', 'shared', 'key', 'value');
+t.set('card', 'shared', 'key', 'value');
+t.set('member', 'shared', 'key', 'value');
+
+If you want to set multiple keys at once you can do that like so
+
+t.set('board', 'shared', { key: value, extra: extraValue });
+
+Reading back your data is as simple as
+
+t.get('organization', 'shared', 'key');
+
+Or want all in scope data at once?
+
+t.getAll();
+
+*/
+var createNewCard = function(listId, card){
+  var key = "e906ee78b84a6e9d04fd467c7e1ae949";
+  var token = "056ad057942dd073e2ecdcd2e8ab18ffd69baad5cfe8c649bb0368d8fc207493";
+  fetch(`https://api.trello.com/1/cards?idList=${listId}&key=${key}&token=${token}&name=${card.name}`,
+    {
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    method: 'POST', 
+    mode: 'cors',
+    redirect: 'follow',
+    referrer: 'no-referrer'
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+  
+};
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+
+var shortListedTasks = [];
+var todoListId = "";
+var boardButtonCallback = function (t, opts) {
+  console.log('Someone clicked the board button');
+  return t.lists('all')
+    .then(function (lists) {
+      console.log(JSON.stringify(lists, null, 2));
+      
+    //get first cards from all GOAL lists
+      lists.forEach((list) => {
+        if(list.name.indexOf("GOAL") != -1){
+          console.log("GOAL LIST - " + list.name);
+          shortListedTasks.push(list.cards.shift());
+        }
+        else if(list.name.indexOf("Todo") != -1){
+          console.log("TODO LIST - " + list.name);
+          todoListId = list.id;
+        }
+      });
+    
+    //create new cards from collected cards
+    shortListedTasks.forEach((card) => { 
+      createNewCard(todoListId,card);
+    })
+    });
+};
+
+// We need to call initialize to get all of our capability handles set up and registered with Trello
+TrelloPowerUp.initialize({
+  // NOTE about asynchronous responses
+  // If you need to make an asynchronous request or action before you can reply to Trello
+  // you can return a Promise (bluebird promises are included at TrelloPowerUp.Promise)
+  // The Promise should resolve to the object type that is expected to be returned
+  'board-buttons': function(t, options){
+    return [{
+      // we can either provide a button that has a callback function
+      // that callback function should probably open a popup, overlay, or boardBar
+      icon: WHITE_ICON,
+      text: 'Get My Tasks',
+      callback: boardButtonCallback
+    }];
+  },
+
+  /*
+
+      🔑 Authorization Capabiltiies 🗝
+
+      The following two capabilities should be used together to determine:
+      1. whether a user is appropriately authorized
+      2. what to do when a user isn't completely authorized
+
+  */
+  'authorization-status': function(t, options){
+    // Return a promise that resolves to an object with a boolean property 'authorized' of true or false
+    // The boolean value determines whether your Power-Up considers the user to be authorized or not.
+
+    // When the value is false, Trello will show the user an "Authorize Account" options when
+    // they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
+    // below determines what should happen when the user clicks "Authorize Account"
+
+    // For instance, if your Power-Up requires a token to be set for the member you could do the following:
+    return t.get('member', 'private', 'token')
+    // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
+    // use t.storeSecret('key', 'value') and t.loadSecret('key')
+    .then(function(token){
+      if(token){
+        return { authorized: true };
+      }
+      return { authorized: false };
+    });
+    // You can also return the object synchronously if you know the answer synchronously.
+  },
+  'show-authorization': function(t, options){
+    // Returns what to do when a user clicks the 'Authorize Account' link from the Power-Up gear icon
+    // which shows when 'authorization-status' returns { authorized: false }.
+
+    // If we want to ask the user to authorize our Power-Up to make full use of the Trello API
+    // you'll need to add your API from trello.com/app-key below:
+    let trelloAPIKey = process.env.TRELLO_API_KEY;
+    // This key will be used to generate a token that you can pass along with the API key to Trello's
+    // RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
+
+    // In this case we'll open a popup to kick off the authorization flow.
+    if (trelloAPIKey) {
+      return t.popup({
+        title: 'My Auth Popup',
+        args: { apiKey: trelloAPIKey }, // Pass in API key to the iframe
+        url: './authorize.html', // Check out public/authorize.html to see how to ask a user to auth
+        height: 140,
+      });
+    } else {
+      console.log("🙈 Looks like you need to add your API key to the project!");
+    }
+  }
+});
+
+console.log('Loaded by: ' + document.referrer);
